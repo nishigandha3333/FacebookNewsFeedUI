bc55053d542688acffa95b2af3f093fd168ef5b8
Nishigandha Dandekar <nishigandha3333@gmail.com>
2021-04-17 11:12:52 -0400

    Initial Project Setup

---
+++ FaceBookNewsFeedUI/FaceBookNewsFeedUI/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FacebookNewsFeedUI
+//
+//  Created by Nishigandha Dandekar on 4/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
